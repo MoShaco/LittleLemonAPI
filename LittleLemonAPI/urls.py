79d27d58d526f51362ff5e0a6bb9dff2fe2e3706
Mohammed Shaco <mohammedshaco@gmail.com>
2024-12-17 09:42:07 +0200

    create two endpoints for 'categories' and link them to their views.

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('categories', views.CategoriesView.as_view(), name="categories"),
+    path('categories/<int:pk>', views.CategoryView.as_view(), name="category"),
+]
