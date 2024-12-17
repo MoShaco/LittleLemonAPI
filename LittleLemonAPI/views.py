6b155c065bc2ea0e940e50b0961933ca38430556
Mohammed Shaco <mohammedshaco@gmail.com>
2024-12-17 09:41:27 +0200

    create two views (CategoriesView, CategoryView) to '/categories' endpoints

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .permissions import IsManager, IsDeliveryCrew, IsManagerOrDeliveryCrew
+from .models import Category
+from .serializers import CategorySerializer
 # Create your views here.
+
+
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method == 'GET':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsManager]
+        return [ permission () for permission in permission_classes]
+
+
+class CategoryView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method == 'GET':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsManager]
+        return [ permission () for permission in permission_classes]
