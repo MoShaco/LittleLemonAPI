2efa6addb71efaf03d5d8d1fee3fbf2656ce55a7
Mohammed Shaco <mohammedshaco@gmail.com>
2024-12-19 10:22:41 +0200

    Create 'MenuItemSerializer' serializer

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, MenuItem
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'slug', 'title']
+
+
+class MenuItemSerializer(serializers.ModelField):
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'title', 'price', 'featured', 'category']
