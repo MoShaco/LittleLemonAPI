30652dd261fb4bce0589156b5fdd05fb270da0c6
Mohammed Shaco <mohammedshaco@gmail.com>
2024-12-19 10:23:34 +0200

    Create two class based views (MenuItemsView, MenuItemView) to hadnle the logic after access /menu-itmes endpoint

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .permissions import IsManager, IsDeliveryCrew, IsManagerOrDeliveryCrew
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, MenuItem
+from .serializers import CategorySerializer, MenuItemSerializer
 # Create your views here.
 
 
@@ -31,3 +31,28 @@ class CategoryView(generics.RetrieveUpdateDestroyAPIView):
         else:
             permission_classes = [IsManager]
         return [ permission () for permission in permission_classes]
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method == 'GET':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsManager]
+        return [permission() for permission in permission_classes]
+
+
+class MenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method == 'GET':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsManager]
+        return [ permission() for permission in permission_classes]
