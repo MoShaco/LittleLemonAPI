f0d8053b0ff10963e71156b357b73f196e64beb9
Mohammed Shaco <mohammedshaco@gmail.com>
2024-12-20 09:22:02 +0200

    and two endpoints in (LittleLemonAPI.urls) for menu-items

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('categories', views.CategoriesView.as_view(), name="categories"),
     path('categories/<int:pk>', views.CategoryView.as_view(), name="category"),
+    path('menu-items', views.MenuItemsView.as_view(), name='menuitems'),
+    path('menu-items/<int:pk>', views.MenuItemView.as_view(), name='manuitem'),
 ]
