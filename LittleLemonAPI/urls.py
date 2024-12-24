7f3d6fe9a6169993605edea8260b0a094080c757
Mohammed Shaco <mohammedshaco@gmail.com>
2024-12-24 11:43:00 +0200

    create endpoints for group managements (manager, delivery-crew)

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('categories/<int:pk>', views.CategoryView.as_view(), name="category"),
     path('menu-items', views.MenuItemsView.as_view(), name='menuitems'),
     path('menu-items/<int:pk>', views.MenuItemView.as_view(), name='manuitem'),
+    path('groups/manager/users', views.ManagerUserView.as_view(), name="Managers"),
+    path('groups/manager/users/<int:pk>', views.ManagerUserView.as_view(), name="Manager"),
+    path('groups/delivery-crew/users', views.DeliveryCrewUserView.as_view(), name="Delivery Crews"),
+    path('groups/delivery-crew/users/<int:pk>', views.DeliveryCrewUserView.as_view(), name="Delivery Crew"),
 ]
